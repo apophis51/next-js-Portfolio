92cef6e6026cd747d8f51397125caa6f6bb4d988
apophis51 <malcolmxvernon@hotmail.com>
2023-09-23 13:29:30 -0400

    Updated Email Functionality

---
+++ app/(email project)/contactEmailAPI/route.js
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server'
+import { sendMail2 } from "../../../service/newmailservice2";
+
+//The POST method submits an entity to the specified resource, often causing a change in state or side effects on the server.
+export async function POST(request) {
+    // await sendMail2();
+    console.log(request)
+    // const formData = await request.json()
+
+    // const formData = await request.json()
+     const formData = await request.formData()
+    // const email = formData.get('email')
+
+
+    // console.log(formData.get('email'))
+    console.log(
+        'name:', formData.get('name'),'\n', 
+        'email:', formData.get('email'), '\n',
+        'message:', formData.get('message')) 
+
+    let name = formData.get('name')
+    let email = formData.get('email')
+    let message = formData.get('message')
+    let transferemail = {
+        name: name,
+        email: email,
+        message: message
+    }
+    await sendMail2(transferemail);
+
+  return NextResponse.json("EMAIL REQUEST RECIEVED")
+}
