2fc5b95b795f5edd77175548aaa6c802f8e1f723
apophis51 <malcolmxvernon@hotmail.com>
2023-11-27 15:36:16 -0500

    Python Snippets Now Correctly Get Colored

--- app/(main site)/PyTest2/page.js
+++ app/(main site)/PyTest2/page.js
@@ -1,5 +1,6 @@
 'use client'
 import {useState,useEffect} from 'react';
+import Container from '@mui/material/Container';
 
 
 
@@ -15,6 +16,8 @@ export default function Home() {
 
       }, [])
     return (
+      <Container maxWidth="xl"  >
+
         <div className ="bg-white">
             <link rel="stylesheet" href="https://pyscript.net/latest/pyscript.css" />
 9        <script defer src="https://pyscript.net/latest/pyscript.js" strategy='beforeInteractive'/>
@@ -22,6 +25,7 @@ export default function Home() {
         dangerouslySetInnerHTML={{__html: pyscript}}
       />
       </div>
+      </Container>
       )
   }
   
