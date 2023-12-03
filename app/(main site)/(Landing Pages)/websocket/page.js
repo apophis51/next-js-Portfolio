76cb3cd6c87629aca9677d8d2be2373cb95349f1
apophis51 <malcolmxvernon@hotmail.com>
2023-12-03 08:34:16 -0500

    another update

--- app/(main site)/(Landing Pages)/websocket/page.js
+++ app/(main site)/(Landing Pages)/websocket/page.js
@@ -1,33 +1,33 @@
 
-// TCP Client
-const net = require('net');
+// // TCP Client
+// const net = require('net');
 
-const client = new net.Socket();
+// const client = new net.Socket();
 
-const PORT = 3001;
-const HOST = 'localhost';
-try {
-// Connect to the server
-client.connect(PORT, HOST, () => {
-  console.log('Connected to server');
+// const PORT = 3001;
+// const HOST = 'localhost';
+// try {
+// // Connect to the server
+// client.connect(PORT, HOST, () => {
+//   console.log('Connected to server');
 
-  // Send data to the server
-  client.write('Hello, Mate!');
-});
-}
-catch(err) {
-  console.log(err);
-}
+//   // Send data to the server
+//   client.write('Hello, Mate!');
+// });
+// }
+// catch(err) {
+//   console.log(err);
+// }
 
-// Handle data received from the server
-client.on('data', data => {
-  console.log(`Received from server: ${data}`);
-});
+// // Handle data received from the server
+// client.on('data', data => {
+//   console.log(`Received from server: ${data}`);
+// });
 
-// Handle server disconnection
-client.on('close', () => {
-  console.log('Connection closed');
-});
+// // Handle server disconnection
+// client.on('close', () => {
+//   console.log('Connection closed');
+// });
 
 export default function socket() {
     return(
