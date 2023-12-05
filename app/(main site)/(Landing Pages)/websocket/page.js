10a7d86cd42f1e141df563d7540559d1fafda4ee
apophis51 <malcolmxvernon@hotmail.com>
2023-12-04 23:58:47 -0500

    update

--- app/(main site)/(Landing Pages)/websocket/page.js
+++ app/(main site)/(Landing Pages)/websocket/page.js
@@ -28,8 +28,39 @@
 // client.on('close', () => {
 //   console.log('Connection closed');
 // });
+const io = require('socket.io-client');
 
-export default function socket() {
+// Replace 'http://localhost:3000' with the actual URL of your Socket.IO server
+// const socket = io('http://localhost:5000');
+const socket = io('https://filereadtest-production.up.railway.app/');
+
+
+// Listen for the 'connect' event, which is emitted when the connection is established
+socket.on('connect', () => {
+  console.log('Connected to Socket.IO server');
+
+  // Emit a 'message' event to the server
+  socket.emit('message', 'Hello from backend!');
+});
+
+// Listen for the 'message' event from the server
+socket.on('message', (data) => {
+  console.log('Message from server:', data);
+
+  socket.on('error', (error) => {
+    console.error('Socket.IO error:', error);
+  });
+
+  // Close the connection after receiving a message (optional)
+  socket.disconnect();
+});
+
+// Listen for the 'disconnect' event, which is emitted when the connection is closed
+socket.on('disconnect', () => {
+  console.log('Disconnected from Socket.IO server');
+});
+
+export default function sockett() {
     return(
         <div className="bg-white">
             <h1>Socket</h1>
