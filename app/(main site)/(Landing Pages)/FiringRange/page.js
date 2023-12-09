32cb38568006e790773e74b0e6a7701d9eb13fde
apophis51 <malcolmxvernon@hotmail.com>
2023-12-09 13:41:19 -0500

    update

--- app/(main site)/(Landing Pages)/FiringRange/page.js
+++ app/(main site)/(Landing Pages)/FiringRange/page.js
@@ -4,15 +4,23 @@ import Hero from '../../Components/Hero'
 // import TerminalComponent from './TerminalComponent.js';
 import TerminalShell from './TerminalShell.js'
 
+import { headers } from 'next/headers'
+
+
 
 export default function FiringRange() {
-
+  const headersList = headers()
+  const referer = headersList.get('referer')
 
   return (
     <Container maxWidth="xl"  >
         <Hero contentNeeded = {"Hacker Firing Range"}/>
         {/* <TerminalComponent /> */}
+        <div>Referer: {referer}</div>
         <TerminalShell />
     </Container>
   )
 }
+
+
+
