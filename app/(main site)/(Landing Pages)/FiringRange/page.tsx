68b9b20aaf3a1c4671b4b14e089d4de4ba6a82b3
apophis51 <malcolmxvernon@hotmail.com>
2023-12-09 13:17:06 -0500

    update

--- app/(main site)/(Landing Pages)/FiringRange/page.tsx
+++ app/(main site)/(Landing Pages)/FiringRange/page.tsx
@@ -6,10 +6,20 @@ import TerminalComponent from './TerminalComponent.js';
 
 
 export default function FiringRange() {
+
+  if (typeof window !== 'undefined') {
+    return ( 
+    <Container maxWidth="xl"  >
+    <Hero contentNeeded = {"Hacker Firing Range"}/>
+    <TerminalComponent />
+</Container>)
+  }
+  else{
   return (
     <Container maxWidth="xl"  >
         <Hero contentNeeded = {"Hacker Firing Range"}/>
         <TerminalComponent />
     </Container>
   )
+  }
 }
