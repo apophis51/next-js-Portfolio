40e1a98ff775274f6d3decc7a9908a17c9845f87
apophis51 <malcolmxvernon@hotmail.com>
2023-12-11 12:04:59 -0500

    update

--- app/(main site)/(Landing Pages)/FiringRange/page.js
+++ app/(main site)/(Landing Pages)/FiringRange/page.js
@@ -1,14 +1,13 @@
 
 import Container from '@mui/material/Container';
 import Hero from '../../Components/Hero'
-import App from './App.js';
-import { headers } from 'next/headers' // this prevents ssr because we had an error reference https://nextjs.org/docs/app/building-your-application/rendering/server-components
+import dynamic from 'next/dynamic'
+
+const App = dynamic(() => import('./App.js'), { ssr: false })
 
 
 
 export default function FiringRange() {
-  const headersList = headers() //prevents ssr
-  const referer = headersList.get('referer')  //prevents ssr
 
   return (
     <Container maxWidth="xl"  >
