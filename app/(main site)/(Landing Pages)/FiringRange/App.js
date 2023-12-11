524107f589e9f7642fb78eeb6728803b3aaf443c
apophis51 <malcolmxvernon@hotmail.com>
2023-12-10 23:10:53 -0500

    update

--- app/(main site)/(Landing Pages)/FiringRange/App.js
+++ app/(main site)/(Landing Pages)/FiringRange/App.js
@@ -1,6 +1,6 @@
 'use client'
 import React, { useEffect, useState } from 'react';
-import TerminalComponent from '../FiringRange/TerminalComponent.js';
+import TerminalComponent from './TerminalComponent.js';
 
 
 const App = () => {
--- app/(main site)/(Landing Pages)/FiringRange/page.js
+++ app/(main site)/(Landing Pages)/FiringRange/page.js
@@ -1,7 +1,7 @@
 
 import Container from '@mui/material/Container';
 import Hero from '../../Components/Hero'
-import TerminalComponent from './TerminalComponent.js';
+import App from './App.js';
 import { headers } from 'next/headers' // this prevents ssr because we had an error reference https://nextjs.org/docs/app/building-your-application/rendering/server-components
 
 
@@ -13,12 +13,11 @@ export default function FiringRange() {
   return (
     <Container maxWidth="xl"  >
         <Hero contentNeeded = {"Hacker Firing Range"}/>
-        <TerminalComponent />
+        <App />
         {/* <div>Referer: {referer}</div> */}
         {/* <TerminalShell /> */}
     </Container>
   )
 }
-
 
 
