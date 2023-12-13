c7de616bea93c70c086aee5b90adbfb1e1c1a017
apophis51 <malcolmxvernon@hotmail.com>
2023-12-13 00:14:31 -0500

    update

--- app/(main site)/Components/Hero.tsx
+++ app/(main site)/Components/Hero.tsx
@@ -18,7 +18,7 @@ async function getData() {
   
     return (
         <div className="hero min-h-screen" style={{ backgroundImage: `url(${Image})` }}>
-          <div className="hero-overlay bg-opacity-80"></div>
+          <div className="hero-overlay bg-opacity-90"></div>
           <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
               <h1 className="mb-5 text-5xl font-bold">{contentNeeded}</h1>
