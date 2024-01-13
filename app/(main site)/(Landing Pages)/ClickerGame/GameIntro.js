42fb5853b087d7d8ed34a05889075ffa74a69e34
apophis51 <malcolmxvernon@hotmail.com>
2024-01-13 04:52:45 -0500

    update

---
+++ app/(main site)/(Landing Pages)/ClickerGame/GameIntro.js
@@ -0,0 +1,29 @@
+`use client`
+
+import React, { useState, useEffect } from 'react';
+
+export default function GameIntro() {
+
+    useEffect(() => {
+        // This will be executed when the component mounts
+        document.getElementById('my_modal_4').showModal();
+      }, [])
+
+    return (
+        <div>
+        {/* <button className="btn" onClick={()=>document.getElementById('my_modal_4').showModal()}>open modal</button> */}
+<dialog id="my_modal_4" className="modal">
+  <div className="modal-box w-11/12 max-w-5xl">
+    <h3 className="font-bold text-lg">Hello!</h3>
+    <p className="py-4">Click the button below to close</p>
+    <div className="modal-action">
+      <form method="dialog">
+        {/* if there is a button, it will close the modal */}
+        <button className="btn">Close</button>
+      </form>
+    </div>
+  </div>
+</dialog>
+</div>
+    )
+}
