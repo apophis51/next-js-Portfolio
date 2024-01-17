16dd198bfce6d083003fc9d84b9ffc34d2da93e7
apophis51 <malcolmxvernon@hotmail.com>
2024-01-17 03:49:44 -0500

    uupdate

--- app/(main site)/(Test pages)/ExportObjectTest/page.js
+++ app/(main site)/(Test pages)/ExportObjectTest/page.js
@@ -1,32 +1,32 @@
 
-class Car {
-    constructor(make, model, year) {
-      this.make = make;
-      this.model = model;
-      this.year = year;
-      console.log(this)
-    }
+// class Car {
+//     constructor(make, model, year) {
+//       this.make = make;
+//       this.model = model;
+//       this.year = year;
+//       console.log(this)
+//     }
   
-    displayInfo() {
-      console.log(`${this.year} ${this.make} ${this.model}`);
-    }
+//     displayInfo() {
+//       console.log(`${this.year} ${this.make} ${this.model}`);
+//     }
   
-    // Serialize the object to JSON
-    toJSON() {
-        return JSON.stringify({
-          make: this.make,
-          model: this.model,
-          year: this.year,
-        });
-      }
-  }
-  const myCar = new Car('Toyota', 'Camry', 2022);  
-  console.log(myCar)
-  let jasonCar = myCar.toJSON();
-  console.log(jasonCar)
+//     // Serialize the object to JSON
+//     toJSON() {
+//         return JSON.stringify({
+//           make: this.make,
+//           model: this.model,
+//           year: this.year,
+//         });
+//       }
+//   }
+//   const myCar = new Car('Toyota', 'Camry', 2022);  
+//   console.log(myCar)
+//   let jasonCar = myCar.toJSON();
+//   console.log(jasonCar)
  
 
-  const reconstructedCar = new Car(...Object.values(JSON.parse(jasonCar)));
+//   const reconstructedCar = new Car(...Object.values(JSON.parse(jasonCar)));
 
-// Use the reconstructed object
-reconstructedCar.displayInfo();
+// // Use the reconstructed object
+// reconstructedCar.displayInfo();
