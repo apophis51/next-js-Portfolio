c608cfbc5623f173e1c2a8ad533442726f95a115
apophis51 <malcolmxvernon@hotmail.com>
2024-01-18 16:35:20 -0500

    update

--- app/(main site)/(Landing Pages)/Crypto/[[...page]]/pageUtils.js
+++ app/(main site)/(Landing Pages)/Crypto/[[...page]]/pageUtils.js
@@ -1,24 +1,52 @@
 import { prisma } from '@/lib/prisma';
+ import {  EpochTime } from '@/app/(main site)/Components/Utils/PartyTime.js'; 
 
-export async function fetchprediction(coin) {
-  let res = await fetch(`https://cryptoai-production.up.railway.app/current/${coin}/1`, { cache: 'no-store' })
-  console.log(res)
+
+ export async function fetchprediction(coin, time='current') {
+  let res = await calculatePrediction(coin, time)
+  let epochTime = new EpochTime();
+   console.log(epochTime.currentEpoch - res.dateUnEdited)
+   if ((epochTime.currentEpoch - res.dateUnEdited) > epochTime.oneDayInMilliseconds) {
+        console.log('fetching new prediction')
+        res = await calculatePrediction(coin, 'new')
+   }
+   updateDatabase(res)
+console.log(res)
+ return (res)
+ }
+
+export async function calculatePrediction(coin, time='current') {
+  let res = await fetch(`https://cryptoai-production.up.railway.app/${time}/${coin}/1`, { cache: 'no-store' })
+  console.log(time)
+ 
+
   console.log('data fetched')
   if (!res.ok) {
+    console.log('failed to fetch')
     throw new Error('Failed to fetch Data');
   }
   return (res.json())
 }
 
-export async function fetchNewPrediction() {
-  console.log('fetching new prediction')
-  let res = await fetch('https://cryptoai-production.up.railway.app/new/ethereum/1', { cache: 'no-store' })
-  console.log('data fetched')
-  if (!res.ok) {
-    throw new Error('Failed to fetch Data');
-  }
-  return (res.json())
-}
+// export async function fetchprediction(coin) {
+//   let res = await fetch(`https://cryptoai-production.up.railway.app/current/${coin}/1`, { cache: 'no-store' })
+//   console.log(res)
+//   console.log('data fetched')
+//   if (!res.ok) {
+//     throw new Error('Failed to fetch Data');
+//   }
+//   return (res.json())
+// }
+
+// export async function fetchNewPrediction() {
+//   console.log('fetching new prediction')
+//   let res = await fetch('https://cryptoai-production.up.railway.app/new/ethereum/1', { cache: 'no-store' })
+//   console.log('data fetched')
+//   if (!res.ok) {
+//     throw new Error('Failed to fetch Data');
+//   }
+//   return (res.json())
+// }
 
 // export async function fetchLastPrediction() {
 //   const lastEntry = await prisma.ethPredictionData.findFirst({
@@ -32,7 +60,7 @@ export async function fetchNewPrediction() {
 
 export async function updateDatabase(data) {
   console.log(data)
-  const smashedDataTypes = data.dateUnEdited +  data.crypto + data.predictionDays
+  const smashedDataTypes = data.dateUnEdited +'$'+  data.crypto +'$'+ data.predictionDays
   console.log(smashedDataTypes)
   try {
     await prisma.cryptoPredictionData.upsert({
