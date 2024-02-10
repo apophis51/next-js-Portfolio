71bece55a031af715ba9b25671e73af8b2c384d7
apophis51 <malcolmxvernon@hotmail.com>
2024-02-10 15:00:26 -0500

    CRYPTOAI UI UPDATeS

--- app/(main site)/(Landing Pages)/Crypto/[[...page]]/CryptoPredictions.js
+++ app/(main site)/(Landing Pages)/Crypto/[[...page]]/CryptoPredictions.js
@@ -14,26 +14,33 @@ export default function CryptoPredictions({ fetchprediction, fetchCryptoPriceDat
   const [gains, setGains] = useState(null)
   const [priceDirection, setPriceDirection] = useState(null)
   const [historicalData, setHistoricalData] = useState(null)
+  const [oneButtonColor, setOneButtonColor] = useState('bg-white')
+  const [tenButtonColor, setTenButtonColor] = useState('bg-white')
 
-  async function fetchHistoricalDataHandler(){
+  async function fetchHistoricalDataHandler() {
     console.log('triggered')
-    let coin =cryptoData.crypto
+    let coin = cryptoData.crypto
     console.log(coin)
     let result = await fetchCryptoPriceData(coin)
     console.log(result)
     setHistoricalData(result)
-  
+
   }
 
 
 
-  
+
 
   async function fetch10PredictionMiddleWare() {
+    //set button color
+    setOneButtonColor('bg-white')
+    setTenButtonColor('bg-lime-400')
+
+
     console.log('triggered')
     let coin = cryptoData.crypto
     let predictiondate = 10
-    let Data10 = await fetchprediction(coin,predictiondate)
+    let Data10 = await fetchprediction(coin, predictiondate)
     console.log(Data10)
     priceDirectionHandler(Data10)
     setCryptoData(Data10)
@@ -41,10 +48,16 @@ export default function CryptoPredictions({ fetchprediction, fetchCryptoPriceDat
   }
 
   async function fetch1PredictionMiddleWare() {
+    //set button color
+    setTenButtonColor('bg-white')
+    setOneButtonColor('bg-lime-400')
+
+
+
     console.log('triggered')
     let coin = cryptoData.crypto
     let predictiondate = 1
-    let Data10 = await fetchprediction(coin,predictiondate)
+    let Data10 = await fetchprediction(coin, predictiondate)
     console.log(Data10)
     priceDirectionHandler(Data10)
     setCryptoData(Data10)
@@ -52,23 +65,23 @@ export default function CryptoPredictions({ fetchprediction, fetchCryptoPriceDat
   }
 
   console.log(gains)
-  async function priceDirectionHandler(data){
-    let priceGain = ((1 - (data.recentprice / data.cryptoprediction))*100).toFixed(2) + '%'
+  async function priceDirectionHandler(data) {
+    let priceGain = ((1 - (data.recentprice / data.cryptoprediction)) * 100).toFixed(2) + '%'
     console.log(priceGain)
     console.log(priceGain + '%')
-    if (priceGain.includes('-')){
+    if (priceGain.includes('-')) {
       setGains(priceGain)
       setPriceDirection('down')
     }
-    else{
+    else {
       setGains(priceGain)
       setPriceDirection('up')
     }
   }
 
-  async function fetchPredictionMiddleWare(coin,predictiondate=1) { 
+  async function fetchPredictionMiddleWare(coin, predictiondate = 1) {
     console.log(coin)
-    let newData = await fetchprediction(coin,predictiondate)
+    let newData = await fetchprediction(coin, predictiondate)
     console.log(newData)
     priceDirectionHandler(newData)
     setCryptoData(newData)
@@ -77,11 +90,11 @@ export default function CryptoPredictions({ fetchprediction, fetchCryptoPriceDat
   const currentTimeStamp = cryptoData?.dateUnEdited
   const currentDate = new Date(currentTimeStamp)
   let targetDate = forcast
-  if (forcast==1){
-  targetDate = new Date(currentDate.getTime() + 1 * 24 * 60 * 60 * 1000);
+  if (forcast == 1) {
+    targetDate = new Date(currentDate.getTime() + 1 * 24 * 60 * 60 * 1000);
   }
-  if (forcast==10){
-  targetDate = new Date(currentDate.getTime() + 1 * 264 * 60 * 60 * 1000);
+  if (forcast == 10) {
+    targetDate = new Date(currentDate.getTime() + 1 * 264 * 60 * 60 * 1000);
 
   }
   let dayOfWeek = targetDate.getDay();
@@ -104,15 +117,15 @@ export default function CryptoPredictions({ fetchprediction, fetchCryptoPriceDat
         <h1 className='text-4xl   md:text-4xl flex items-center justify-center p-10 '>Crypto Predictions - Beta</h1>
       </div>
       <div className='flex items-center justify-center'>
-      {(historicalData) && (<GoogleCryptoChart historicalData={historicalData} cryptoData={cryptoData?.crypto} />)}
+        {(historicalData) && (<GoogleCryptoChart historicalData={historicalData} cryptoData={cryptoData?.crypto} />)}
       </div>
       <br></br>
       <div className='flex items-center justify-center'>
         <SelectButton selectionmenu={selections} setFilterFunction={fetchPredictionMiddleWare} />
       </div>
       <div className='flex items-center justify-center gap-5 mb-3 flex-wrap'>
-        <button className='btn' onClick={fetch1PredictionMiddleWare}>One Day ForeCast</button>
-        <button className='btn' onClick={fetch10PredictionMiddleWare}>Ten Day ForeCast</button>
+        <button className={`btn ${oneButtonColor}`} onClick={fetch1PredictionMiddleWare}>One Day ForeCast</button>
+        <button className={`btn ${tenButtonColor}`} onClick={fetch10PredictionMiddleWare}>Ten Day ForeCast</button>
       </div>
 
       <div className="stats shadow flex flex-col items-center justify-center ml-[1%] mr-[1%] mt-[2%] md:ml-[30%] md:mr-[30%] xl:pl-[5%] xl:pr-[15%] ">
@@ -151,9 +164,9 @@ export default function CryptoPredictions({ fetchprediction, fetchCryptoPriceDat
 
       {/* break */}
 
-<div className='flex flex-col items-center gap-10 justify-center pl-[10%] pr-[10%] pb-[10%] text-lg'>
-  <div><p>By {day} {cryptoData?.crypto} is predicted to be worth ${cryptoData?.cryptoprediction.toFixed(2)} U.S Dollars</p></div>
-  <div><p><b>This is Not Financial Advice</b></p></div>
+      <div className='flex flex-col items-center gap-10 justify-center pl-[10%] pr-[10%] pb-[10%] text-lg'>
+        <div><p>By {day} {cryptoData?.crypto} is predicted to be worth ${cryptoData?.cryptoprediction.toFixed(2)} U.S Dollars</p></div>
+        <div><p><b>This is Not Financial Advice</b></p></div>
       </div>
     </div>
   );
