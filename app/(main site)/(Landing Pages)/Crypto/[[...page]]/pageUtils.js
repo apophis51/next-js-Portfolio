0aa912fc94ed8e03be3b9833c69de49fbe463ce6
apophis51 <malcolmxvernon@hotmail.com>
2024-02-10 18:25:17 -0500

    database deactivation

--- app/(main site)/(Landing Pages)/Crypto/[[...page]]/pageUtils.js
+++ app/(main site)/(Landing Pages)/Crypto/[[...page]]/pageUtils.js
@@ -1,3 +1,5 @@
+
+//renable database later
 "use server"
 
 import { prisma } from '@/lib/prisma';
@@ -42,7 +44,7 @@ import { prisma } from '@/lib/prisma';
         res = await fetch('https://cryptoai-production.up.railway.app/updateAll', { cache: 'no-store' })
         return (null)
    }
-   updateDatabase(res)
+  //  updateDatabase(res)
 console.log(res)
  return (res)
  }
