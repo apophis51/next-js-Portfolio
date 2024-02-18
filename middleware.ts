afc676d6514ac626226fcddc56fd2e86188dee24
apophis51 <malcolmxvernon@hotmail.com>
2024-02-17 23:53:51 -0500

    update

--- middleware.ts
+++ middleware.ts
@@ -5,41 +5,41 @@ import type { NextRequest } from 'next/server'
 import { authMiddleware } from "@clerk/nextjs";
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  if(request.nextUrl.pathname.startsWith('/api/auth/callback/credentials')) {
-  return NextResponse.rewrite(new URL('/authentication/auth/callback/credentials', request.url))
-  }
-  if(request.nextUrl.pathname.startsWith('/api/auth/session')) {
-    return NextResponse.rewrite(new URL('/authentication/auth/session', request.url))
-    }
-    if(request.nextUrl.pathname.startsWith('/api/auth/_log')) {
-      return NextResponse.rewrite(new URL('/authentication/auth/_log', request.url))
-      }
-      if(request.nextUrl.pathname.startsWith('/api/auth/error')) {
-        return NextResponse.rewrite(new URL('/authentication/auth/error?error=CredentialsSignin&provider=credentials', request.url))
-        }
-        if(request.nextUrl.pathname.startsWith('/api/auth/signin/github')) {
-          return NextResponse.rewrite(new URL('/authentication/auth/signin/github', request.url))
-          }
-          if(request.nextUrl.pathname.startsWith('/api/auth/callback/github')) {
-            return NextResponse.rewrite(new URL('/authentication/auth/callback/github', request.url))
-            }
-            if(request.nextUrl.pathname.startsWith('/api/auth/signin/google')) {
-              return NextResponse.rewrite(new URL('/authentication/auth/signin/google', request.url))
-              }
-              if(request.nextUrl.pathname.startsWith('/api/auth/callback/google')) {
-                return NextResponse.rewrite(new URL('/authentication/auth/callback/google', request.url))
-                }
-        if(request.nextUrl.pathname.startsWith('/api/auth/signin')) {
-          return NextResponse.rewrite(new URL('/authentication/auth/signin?error=CredentialsSignin', request.url))
-          }
-          if(request.nextUrl.pathname.startsWith('/api/auth/signout')) {
-            return NextResponse.rewrite(new URL('/authentication/auth/signout', request.url))
-            }
-            if(request.nextUrl.pathname.startsWith('/api/auth/providers')) {
-              return NextResponse.rewrite(new URL('/authentication/auth/providers', request.url))
-              }
-}
+// export function middleware(request: NextRequest) {
+//   if(request.nextUrl.pathname.startsWith('/api/auth/callback/credentials')) {
+//   return NextResponse.rewrite(new URL('/authentication/auth/callback/credentials', request.url))
+//   }
+//   if(request.nextUrl.pathname.startsWith('/api/auth/session')) {
+//     return NextResponse.rewrite(new URL('/authentication/auth/session', request.url))
+//     }
+//     if(request.nextUrl.pathname.startsWith('/api/auth/_log')) {
+//       return NextResponse.rewrite(new URL('/authentication/auth/_log', request.url))
+//       }
+//       if(request.nextUrl.pathname.startsWith('/api/auth/error')) {
+//         return NextResponse.rewrite(new URL('/authentication/auth/error?error=CredentialsSignin&provider=credentials', request.url))
+//         }
+//         if(request.nextUrl.pathname.startsWith('/api/auth/signin/github')) {
+//           return NextResponse.rewrite(new URL('/authentication/auth/signin/github', request.url))
+//           }
+//           if(request.nextUrl.pathname.startsWith('/api/auth/callback/github')) {
+//             return NextResponse.rewrite(new URL('/authentication/auth/callback/github', request.url))
+//             }
+//             if(request.nextUrl.pathname.startsWith('/api/auth/signin/google')) {
+//               return NextResponse.rewrite(new URL('/authentication/auth/signin/google', request.url))
+//               }
+//               if(request.nextUrl.pathname.startsWith('/api/auth/callback/google')) {
+//                 return NextResponse.rewrite(new URL('/authentication/auth/callback/google', request.url))
+//                 }
+//         if(request.nextUrl.pathname.startsWith('/api/auth/signin')) {
+//           return NextResponse.rewrite(new URL('/authentication/auth/signin?error=CredentialsSignin', request.url))
+//           }
+//           if(request.nextUrl.pathname.startsWith('/api/auth/signout')) {
+//             return NextResponse.rewrite(new URL('/authentication/auth/signout', request.url))
+//             }
+//             if(request.nextUrl.pathname.startsWith('/api/auth/providers')) {
+//               return NextResponse.rewrite(new URL('/authentication/auth/providers', request.url))
+//               }
+// }
  
 // See You can configure middle where with config or with conditioonal statements https://nextjs.org/docs/app/building-your-application/routing/middleware
 // export const config = {
