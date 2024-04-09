c6800ddb10f4166527c83bf43c009765b2cc093a
apophis51 <malcolmxvernon@hotmail.com>
2024-04-09 04:06:17 -0400

    update

--- app/(main site)/PurchaseMenu/API/route.js
+++ app/(main site)/PurchaseMenu/API/route.js
@@ -51,7 +51,8 @@ export async function POST(data) {
     return NextResponse.json(
         {
             data: {
-                information: 'Your job application has been submitted. Thank you for using WorkSearchApp.'
+                information: 'Your job application has been submitted. Thank you for using WorkSearchApp.',
+                logs: paymentIntentSucceeded.ammount
             }
         },
         responseUtils.allowCors)
