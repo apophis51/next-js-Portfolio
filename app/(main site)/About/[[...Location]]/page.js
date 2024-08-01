0fbc64d9a8002ee414c72a99dc69c906658f0963
apophis51 <malcolmxvernon@hotmail.com>
2024-08-01 07:22:48 -0400

    update

--- app/(main site)/About/[[...Location]]/page.js
+++ app/(main site)/About/[[...Location]]/page.js
@@ -5,15 +5,16 @@ import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import About from '../About'
-//import BottomNavBar from './BottomNavBar'; deleted 8/1/2024
+import BottomNavBar from './BottomNavBar'
 // import '../markdown.css'  deleted 4/20/2024
 import ProjectCards from '../../ProjectCards/page';
 import Container from '@mui/material/Container';
-import dynamic from 'next/dynamic'
+// import dynamic from 'next/dynamic'
 
 
 import JumboTron from '../../JumboTron/page'
-const BottomNavBar = dynamic(() => import('./BottomNavBar'), { ssr: false })
+//const BottomNavBar = dynamic(() => import('./BottomNavBar'), { ssr: false })
+export const dynamic = 'force-dynamic'
 
 
 
