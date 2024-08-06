4dcaa56f192d2d6738eb5d43e28d875f5d5d170c
apophis51 <malcolmxvernon@hotmail.com>
2024-08-06 02:25:03 -0400

    update

--- app/(main site)/(Test pages)/Opperation-Menu/page.tsx
+++ app/(main site)/(Test pages)/Opperation-Menu/page.tsx
@@ -1,14 +1,14 @@
 'use client'
 import { useState } from "react";
 
-const dummyAssignees = ["Jack", "Emily", "Jon", "TBD"];
-const dummyVettingSubmissions = [
-    {},
+let dummyAssignees = ["Jack", "Emily", "Jon", "TBD"];
+let dummyVettingSubmissions = [
+
     {
         Client: "Malcolm",
         Page: "Open",
         Urgent: "Yes",
-        Completed: "No",
+        Completed: "Yes",
         Assignee: "Jack",
     },
     {
@@ -29,7 +29,7 @@ const dummyVettingSubmissions = [
         Client: "Legacy",
         Page: "Closed",
         Urgent: "Yes",
-        Completed: "yes",
+        Completed: "Yes",
         Assignee: "Jon",
     },
     {
@@ -69,68 +69,100 @@ const dummyVettingSubmissions = [
     },
 ]
 
-
+console.log('hit')
 export default function VettingSubmissionFeed() {
     const [vettingSubmissions, setVettingSubmissions] = useState(dummyVettingSubmissions)
+    const [submissionsToggled, setSubmissionsToggled] = useState(false)
+
+    function handleClientSort(Column: string) {
+        if (submissionsToggled) {
+            setSubmissionsToggled(false)
+            dummyVettingSubmissions.sort((a, b) => {
+                if (a[Column] < b[Column]) {
+                    return -1;
+                }
+                if (a[Column] > b[Column]) {
+                    return 1;
+                }
+                return 0;
+            })
+        }
+        else {
+            setSubmissionsToggled(true)
+            dummyVettingSubmissions.sort((a, b) => {
+                if (a[Column] < b[Column]) {
+                    return 1;
+                }
+                if (a[Column] > b[Column]) {
+                    return -1;
+                }
+                return 0;
+            })
+
+        }
+
+        console.log(dummyVettingSubmissions)
+        setVettingSubmissions(dummyVettingSubmissions)
+    }
 
     return (
         <div className="ml-[14%] mr-[14%]">
-            <div className="flex flex-col gap-4 justify-center items-center bg-white p-7">
+            <div className="flex flex-col gap-4 justify-between items-center bg-white p-7">
                 <h1 className=" text-3xl text-center p-5 font-bold">Vetting Submission Feed</h1>
 
                 <input type="text" placeholder="Search" className="input input-bordered w-full max-w-xs" />
-                
-                <div className='flex flex-row  justify-evenly items-center w-[730px]'>
-                    
 
-                <div className='flex items-center gap-1'>
-                        <p className=" text-center">Client</p>
-                        <button className='bg-orange-200 rounded-lg p-1'>
+                <div className='flex flex-row gap-5 justify-between items-center w-[700px]'>
+
+
+                    <div className='flex justify-center items-center gap-1 w-[150px]'>
+                        <p className="text-center" >Client</p>
+                        <button onClick={() => handleClientSort('Client')} className='bg-orange-200 rounded-lg btn btn-xs'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 h-4 w-4 ">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
-                        </svg>
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
+                            </svg>
                         </button>
                     </div>
 
-                    <div className='flex items-center gap-1'>
+                    <div className='flex items-center gap-1 w-[100px]'>
                         <p className=" text-center">Page</p>
-                        <button className='bg-orange-200 rounded-lg p-1'>
+                        <button onClick={() => handleClientSort('Page')} className='bg-orange-200 rounded-lg btn btn-xs'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 h-4 w-4 ">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
-                        </svg>
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
+                            </svg>
                         </button>
                     </div>
 
                     <div className='flex items-center gap-1'>
                         <p className=" text-center">Urgent</p>
-                        <button className='bg-orange-200 rounded-lg p-1'>
+                        <button onClick={() => handleClientSort('Urgent')} className='bg-orange-200 rounded-lg btn btn-xs'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 h-4 w-4 ">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
-                        </svg>
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
+                            </svg>
                         </button>
                     </div>
 
                     <div className='flex items-center gap-1'>
                         <p className=" text-center">Completed</p>
-                        <button className='bg-orange-200 rounded-lg p-1'>
+                        <button onClick={() => handleClientSort('Completed')} className='bg-orange-200 rounded-lg btn btn-xs'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 h-4 w-4 ">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
-                        </svg>
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
+                            </svg>
                         </button>
                     </div>
 
 
                     <div className='flex items-center gap-1'>
-                        <p className=" text-center">Assignee</p>
-                        <button className='bg-orange-200 rounded-lg p-1'>
+                        <p className=" text-center w-[80px]">Assignee</p>
+                        <button onClick={() => handleClientSort('Assignee')} className='bg-orange-200 rounded-lg btn btn-xs'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 h-4 w-4 ">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
-                        </svg>
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
+                            </svg>
                         </button>
                     </div>
-                    
+
                 </div>
-                {dummyVettingSubmissions.map((sub, index) => (
+                {vettingSubmissions.map((sub, index) => (
 
                     <div className='flex flex-row gap-15 justify-between items-center w-[700px]' key={index}>
                         <p className="btn w-[150px] bg-orange-200">{sub.Client}</p>
