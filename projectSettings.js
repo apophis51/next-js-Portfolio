91816b6ee494b3cbb0ffe8e862c671966b9913a2
apophis51 <malcolmxvernon@hotmail.com>
2024-10-31 07:21:04 -0400

    uploadmongodata

--- projectSettings.js
+++ projectSettings.js
@@ -6,7 +6,8 @@ export default function projectURLS() {
       envUtilsWebSocket: 'ws://localhost:3532',    //Crypto AI must be running for this to work
       GoogleTableChartSocket: 'ws://localhost:3532',
       blogsURL: 'https://malcmind-strapi-cms-production.up.railway.app/api/programming-blogs?pagination[page]=1&pagination[pageSize]=8000',
-      pythonMongoDBServer: 'http://127.0.0.1:8000/api/get-all-blogs'
+      pythonMongoDBServer: 'http://127.0.0.1:8000/api/get-all-blogs',
+      pythonMongoDBServerAddBlog: 'http://127.0.0.1:8000/api/add-blog'
     }
   }
   else {
@@ -15,7 +16,8 @@ export default function projectURLS() {
       envUtilsWebSocket: 'wss://cryptoai-production.up.railway.app',
       GoogleTableChartSocket: 'wss://cryptoai-production.up.railway.app',
       blogsURL: 'https://malcmind-strapi-cms-production.up.railway.app/api/programming-blogs?pagination[page]=1&pagination[pageSize]=8000',
-      pythonMongoDBServer :'https://fastapi-mongo-production.up.railway.app/api/get-all-blogs'
+      pythonMongoDBServer :'https://fastapi-mongo-production.up.railway.app/api/get-all-blogs',
+      pythonMongoDBServerAddBlog: 'https://fastapi-mongo-production.up.railway.app/api/add-blog'
     }
   }
 
