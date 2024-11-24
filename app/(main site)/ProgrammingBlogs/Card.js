164056020e592a90298789200aa22d6bf24480d0
apophis51 <malcolmxvernon@hotmail.com>
2024-11-23 19:58:55 -0500

    updated flex spacing on blogs

--- app/(main site)/ProgrammingBlogs/Card.js
+++ app/(main site)/ProgrammingBlogs/Card.js
@@ -1,37 +1,29 @@
-
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import Link from 'next/link'
-
-
+import Link from 'next/link';
 
 export default function MediaCard(props) {
-    const{ blogTitle, blogLink, blogContent} = props
+  const { blogTitle, blogLink, blogContent } = props;
   return (
-    <div className='shadow-[0px_0px_10px_1px_rgba(255,255,255,0.5)]'>
-    <Card sx={{ maxWidth: [300,400], //mui uses breakpoints
-                maxHeight: 720, //used to be 620
-                minHeight: 720, //used to be 620
-                Display: 'flex',
-                }}>
-      <CardContent>
-        {/* <div className ='bg-blue-900'>
-        <Typography gutterBottom variant="h5" component="div">
-          {blogTitle}
-        </Typography>
-        </div> */}
-        <Typography variant="body2" color="text.secondary">
-          <Link href = {blogLink}>{blogContent}</Link>
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" ><Link href = {blogLink}>Learn More</Link></Button>
-      </CardActions>
-    </Card>
+    <div className="shadow-[0px_0px_10px_1px_rgba(255,255,255,0.5)]">
+      <Card className="flex flex-col max-w-[400px] min-h-[720px] max-h-[720px]">
+        {/* Content Section */}
+        <CardContent className="flex-grow">
+          <Typography variant="body2" color="text.secondary">
+            <Link href={blogLink}>{blogContent}</Link>
+          </Typography>
+        </CardContent>
+
+        {/* Actions Section */}
+        <CardActions className="mt-auto flex justify-end">
+          <Button size="small">
+            <Link href={blogLink}>Learn More</Link>
+          </Button>
+        </CardActions>
+      </Card>
     </div>
   );
 }
