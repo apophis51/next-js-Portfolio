31ae208d41facfb28a660e845cfde5dc15869c24
apophis51 <malcolmxvernon@hotmail.com>
2024-11-24 05:15:55 -0500

    trimmed code on edit markdown

--- app/(main site)/(Landing Pages)/AdminDash/[ID]/EditMarkdown.tsx
+++ app/(main site)/(Landing Pages)/AdminDash/[ID]/EditMarkdown.tsx
@@ -2,13 +2,7 @@
 
 
 "use client";
-// import  ReactMarkdown  from "react-markdown"
-import ReactMarkdown from 'react-markdown';
 import Container from '@mui/material/Container';
-import { mongoDBDownloadAtom } from '../globalAdminDashAtoms'
-import { atom, useAtom } from 'jotai'
-import projectURLS from '@/projectSettings'
-import { useEffect,useState } from 'react'
 import { HighlightafterEveryRender } from '@/app/(main site)/Components/Utils/highlighter'
 
 import SimpleMDE from "react-simplemde-editor";
@@ -16,36 +10,9 @@ import "easymde/dist/easymde.min.css";
 import '@/app/(main site)/Components/styles/prism.css'
 
 
-//import params
+export function EditMarkdown({ Content, onChange }) {
 
 
-
-export  function EditMarkdown({Content, onChange}) {
-
-  // const ID = '67239574d4cf31f65e1daa41'
-  // console.log(ID)
-
-
-  const [editContent, setEditContent] = useState(Content)
-
-  // console.log(downloadedBlog)
-
-  // async function serverGetBlogs() {
-
-  //   console.log('about to fetch serverBlogs')
-  //   const res = await fetch(projectURLS().pythonMongoDBServer)
-  //   console.log('we just got a res response')
-  //   const data = await res.json()
-  //   console.log(data)
-  //   setValue(data.find((blog) => blog.id == ID).MarkdownContent)
-  //   console.log(data)
-  // }
-
-  // useEffect(() => {
-
-  //     serverGetBlogs()
-    
-  // }, [])
   HighlightafterEveryRender()
 
 
@@ -54,10 +21,10 @@ export  function EditMarkdown({Content, onChange}) {
       <div className='bg-white p-9  flex-col  md:flex md:flex-row md:overflow-visible items-center justify-evenly overflow-y-hidden overflow-x-hidden'>
         <div className='prose prose-sm lg:prose-xl prose-a:text-red-600'>
           {
-          <>
-            <SimpleMDE value={Content} onChange={onChange} />
-          </>
-           }
+            <>
+              <SimpleMDE value={Content} onChange={onChange} />
+            </>
+          }
         </div>
       </div>
     </Container>)
