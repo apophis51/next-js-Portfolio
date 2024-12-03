649ddfcc075c00e087421896e57f2de5ee1e7db0
apophis51 <malcolmxvernon@hotmail.com>
2024-12-03 04:48:44 -0500

    vcvc

--- app/(main site)/Components/Section.tsx
+++ app/(main site)/Components/Section.tsx
@@ -15,7 +15,7 @@ export async function Section({getData,sectionTitle}: SectionType){
     return (
       <div className='bg-gradient-to-tr from-purple-600 to-blue-900 mt-5 flex-col flex items-center justify-center gap-5 p-6 shadow-[0px_0px_10px_3px_rgba(255,255,255,0.5)]'>
         <h2 className=" text-5xl text-white p-5">{sectionTitle}</h2>
-            <div className='  flex flex-col gap-5 '>
+            <div className='  flex flex-col gap-5'>
               {getData.map((item: UniversalData) => (
                 <div className="card lg:card-side bg-base-100 shadow-xl lg:max-h-64">
                   <figure className='max-w-xs min-w-fit pt-7 lg:pt-10 lg:pb-10 lg:pl-10'><img src={item.Image} alt="Album" /></figure>
