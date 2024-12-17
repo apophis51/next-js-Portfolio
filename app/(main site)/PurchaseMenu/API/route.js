400bad45e04a288820fd6ee61aa3b3ee5a473f03
apophis51 <malcolmxvernon@hotmail.com>
2024-12-17 03:50:51 -0500

    confirming result

--- app/(main site)/PurchaseMenu/API/route.js
+++ app/(main site)/PurchaseMenu/API/route.js
@@ -34,8 +34,8 @@ export async function POST(data) {
 
     try {
         // Verify the Stripe signature
-        // const rawBody = await getRawBody(data.body);
-        const rawBody = await data.text()
+        const rawBody = await getRawBody(data.body);
+        // const rawBody = await data.text()
         event = stripe.webhooks.constructEvent(rawBody, sig, endpointSecret);
 
         // Handle the event
