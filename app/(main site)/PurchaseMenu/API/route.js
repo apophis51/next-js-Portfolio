ac229f6c9cb94cff4f6f6cee335a8a4d755396a0
apophis51 <malcolmxvernon@hotmail.com>
2024-12-17 00:44:37 -0500

    trying to troubleshoot  webhook error still

--- app/(main site)/PurchaseMenu/API/route.js
+++ app/(main site)/PurchaseMenu/API/route.js
@@ -5,8 +5,8 @@ import * as responseUtils from '@/app/(main site)/(Landing Pages)/Work-Search-Ap
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-// const endpointSecret = "whsec_7d827d24ac63d8ed989a538f76c3136f59dbd55a2ee8835069bc186c46598194";
-const endpointSecret = "we_1P3ZQWCBn6LU6bJVdFFDpEhB";
+ const endpointSecret = "whsec_7d827d24ac63d8ed989a538f76c3136f59dbd55a2ee8835069bc186c46598194";
+//const endpointSecret = "we_1P3ZQWCBn6LU6bJVdFFDpEhB";
 
 async function getRawBody(readable) {
     const chunks = [];
@@ -24,8 +24,9 @@ export async function POST(data) {
 
 
     let event = ''
-    let body = await data.body
-    const rawBody = await getRawBody(body)
+    // let body = await data.body
+    // const rawBody = await getRawBody(body)
+    const rawBody = data
     // body = await body.toString()
 
     console.log(rawBody)
