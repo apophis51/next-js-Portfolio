c687bfc8c2c40b6ee53af8359a594072ff76d187
apophis51 <malcolmxvernon@hotmail.com>
2024-12-19 01:47:28 -0500

    troubleshootngfailed hook

--- app/(main site)/PurchaseMenu/checkoutroute/route.js
+++ app/(main site)/PurchaseMenu/checkoutroute/route.js
@@ -30,7 +30,7 @@ export async function POST(data) {
                 },
             ],
             metadata: {
-                userId: userId,
+                userId: 'testlkjlajf'
               },
             // mode: 'payment',
             // mode: 'subscription',
