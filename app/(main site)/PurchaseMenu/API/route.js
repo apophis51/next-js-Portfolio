60a46ff0c5bf9c6a1f119a7b4a01e9049efa61ad
apophis51 <malcolmxvernon@hotmail.com>
2024-12-19 03:33:31 -0500

    trying to solve data saving to clerk object for userid purchased now

--- app/(main site)/PurchaseMenu/API/route.js
+++ app/(main site)/PurchaseMenu/API/route.js
@@ -58,7 +58,7 @@ export async function POST(data) {
             case 'checkout.session.completed':
                 const metadata = event.data.object.metadata;
                 console.log('Metadata:', metadata);
-                createNewMetaData('Ai_credits_granted', 'yes', metadata.userId)
+                await createNewMetaData('Ai_credits_granted', 'yes', metadata.userId)
                 // Return a 200 status to acknowledge the request
                 return NextResponse.json(
                     {
