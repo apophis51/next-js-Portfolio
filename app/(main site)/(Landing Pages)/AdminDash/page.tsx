ead7602ec49e287eaa8fc436b8762f72c0da4a97
apophis51 <malcolmxvernon@hotmail.com>
2024-12-22 23:18:21 -0500

    fixed tabview css almost 100%

--- app/(main site)/(Landing Pages)/AdminDash/page.tsx
+++ app/(main site)/(Landing Pages)/AdminDash/page.tsx
@@ -8,44 +8,51 @@ import BlogRenderHorizontal from "./BlogRenderHorizontal"
 import BlogRenderConstructionBlogs from "./BlogRenderConstrucitonBlogs";
 import CreateANewBlog from "./CreateANewBlog/page";
 import TabView from "../../Components/ui/TabView";
+import TabView2 from "@/app/(main site)/Components/ui/TabView2";
 import Container from '@mui/material/Container';
 import ClerkTester from "@/app/(main site)/(Landing Pages)/AdminDash/ClerkTester/ClerkTester";
 
 
 export default function AdminDash() {
 
-  
+
     return (
-        <Container maxWidth="xl"  >   
-        <div className="flex flex-col gap-4 overflow-x-hidden">
-            
-            <div>
-                <AIArticleGenerator titleName="MalcMind - AI Article Generator" />
-            </div>
-            <div>
-                <Obsidian />
-            </div>
-            {/* <div>
+        <Container maxWidth="xl"  >
+            <div className="flex flex-col gap-4 overflow-x-hidden relative ">
+
+                <div className="">
+                    <AIArticleGenerator titleName="MalcMind - AI Article Generator" />
+                </div>
+                <div>
+                    <Obsidian />
+                </div>
+                {/* <div>
                 <BlogRenderHorizontal />
-                </div>*/} 
-            <div>
-                <BlogRenderConstructionBlogs /> 
-            </div> 
-            <div className="">
-                <TabView TabContent= {{"All Blogs": <BlogRenderHorizontal />, Construction: <BlogRenderConstructionBlogs />}}/>
-            </div>
-            <div>
-                <CreateANewBlog /> 
-            </div>
-            <div>
-                <ClerkTester />
-            </div>
-            <div>
-                <Emails />
-            </div>
+                </div>*/}
+                <div>
+                    <BlogRenderConstructionBlogs />
+                </div>
+                <div className="">
+                    <div className=' w-full min-h-[100vh]  bg-red-400 grow'>
+                    {/* <BlogRenderConstructionBlogs /> */}
+                        <TabView TabContent={{ "All Blogs": <BlogRenderHorizontal />, Construction: <BlogRenderConstructionBlogs /> }} />
+                    </div>
+                </div>
+                {/* <div> 
+                    <TabView2 />
+                </div> */}
+                <div>
+                    <CreateANewBlog />
+                </div>
+                <div>
+                    <ClerkTester />
+                </div>
+                <div>
+                    <Emails />
+                </div>
 
 
-        </div>
+            </div>
         </Container>
     );
 }
--- app/(main site)/Components/ui/TabView.tsx
+++ app/(main site)/Components/ui/TabView.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export default function TabView({ TabContent }: { TabContent: { [key: string]: ReactNode } }) {
     return (
 
-        <div role="tablist" className="tabs tabs-lifted  bg-white">
+        <div role="tablist" className="tabs tabs-lifted  bg-blue-500  max-w-[vw]">
 
             {Object.entries(TabContent).map(([key, value],index) => {
                 //console.log(index)
@@ -18,8 +18,8 @@ export default function TabView({ TabContent }: { TabContent: { [key: string]: R
                         aria-label={key}
                         defaultChecked = {index === 0}
                         />
-                    <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6  ">
-                        {value}
+                    <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box max-w-[vw]">
+                        <div className="absolute inset-x-0">{value}</div>
                     </div>
                 </>)})
             }
