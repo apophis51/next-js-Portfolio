671ee31748232c89138209ccfce3892bc3db1fce
apophis51 <malcolmxvernon@hotmail.com>
2025-01-09 02:08:11 -0500

    updated cms

--- app/(main site)/(Landing Pages)/Web-Apps/ai-article-generator/page.tsx
+++ app/(main site)/(Landing Pages)/Web-Apps/ai-article-generator/page.tsx
@@ -2,10 +2,6 @@
 export const dynamic = 'force-dynamic'
 
 import AIArticleGenerator from "@/app/(main site)/(Landing Pages)/ai-article-generator/AiArticleGenerator"
-import BlogRenderHorizontal from "@/app/(main site)/(Landing Pages)/AdminDash/BlogRenderHorizontal"
-import BlogRenderConstructionBlogs from "@/app/(main site)/(Landing Pages)/AdminDash/BlogRenderConstrucitonBlogs";
-import CreateANewBlog from "@/app/(main site)/(Landing Pages)/AdminDash/CreateANewBlog/page";
-import TabView from "@/app/(main site)/Components/ui/TabView";
 import Container from '@mui/material/Container';
 
 
@@ -17,12 +13,6 @@ export default function AdminDash() {
         <div className="flex flex-col gap-4 overflow-x-hidden">
             <div>
                 <AIArticleGenerator titleName="MalcMind - AI Article Generator" AI_product_name="MalcMind AI"/>
-            </div>
-            <div className="">
-              {/* <TabView TabContent= {{"All Blogs": <BlogRenderHorizontal />, Construction: <BlogRenderConstructionBlogs />}}/> */}
-            </div>
-            <div>
-                {/* <CreateANewBlog />  */}
             </div>
         </div>
         </Container>
