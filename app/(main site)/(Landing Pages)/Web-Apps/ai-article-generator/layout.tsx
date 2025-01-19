221143b47e85a7254f5f1a03edf62d77c5442adf
apophis51 <malcolmxvernon@hotmail.com>
2025-01-19 04:23:27 -0500

    fsddsssssssssssss

---
+++ app/(main site)/(Landing Pages)/Web-Apps/ai-article-generator/layout.tsx
@@ -0,0 +1,26 @@
+'use server'
+
+import { headers } from "next/headers";
+import {getOneContent} from '@/app/(main site)/Components/db_services/mongo'
+
+export async function generateMetadata() {  
+
+    const header = await headers()
+    const xurl = header.get('x-url');
+    const path = xurl!.split('/').pop()
+
+    const data = await getOneContent(path,"meta")
+     
+    return {
+      title: data.Title,
+      description: data.Description 
+    }
+  }
+
+  export default async function BlogLayout({ children }: { children: React.ReactNode }) {
+    return (
+      <>
+        {children}
+      </>
+    )
+  }
