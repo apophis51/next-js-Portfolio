462132695e14d5bb05337d711664aba12cbd54b5
apophis51 <malcolmxvernon@hotmail.com>
2025-01-27 01:32:44 -0500

    update

--- app/(main site)/(Landing Pages)/AdminDash/ContentRenderUniveseral2.tsx
+++ app/(main site)/(Landing Pages)/AdminDash/ContentRenderUniveseral2.tsx
@@ -13,7 +13,7 @@ import { CloseButton } from '@/public/utils/CloseButton'
 import Link from 'next/link'
 import { mongoDBDownloadAtom } from './globalAdminDashAtoms'
 import useAdvancedSelect from '@/app/(main site)/Components/ui/AdvancedSelect'
-import {getALLUserBlogs} from '@/app/(main site)/Components/db_services/mongo'
+import { getALLUserBlogs } from '@/app/(main site)/Components/db_services/mongo'
 
 
 
@@ -28,7 +28,7 @@ interface Blog {
 }
 
 
-export default function ContentRenderUniversal2({ contentType, settings, user }: { contentType: string, settings: string[] , user: string }) {
+export default function ContentRenderUniversal2({ contentType, settings, user }: { contentType: string, settings: string[], user: string }) {
     console.log(contentType)
     console.log(settings)
     console.log('attempting to render Horizontal Blogs')
@@ -86,7 +86,7 @@ export default function ContentRenderUniversal2({ contentType, settings, user }:
         }
     }, [articleAccumulator])
 
-console.log('triggered')
+    console.log('triggered')
     return (
         <MainContentTemplate title={`My ${contentType.replace(/^./, char => char.toUpperCase())} Content`}>
             <div className="mb-5 max-w-xs mx-auto"><CategorySelect /></div>
@@ -105,43 +105,43 @@ console.log('triggered')
                         className='flex flex-row justify-between items-center gap-2 min-h-[70vh] min-w-[65vw] bg-green-400 overflow-x-auto px-4 ' >
                         {/* <p className='bg-white text-black  '>We Could Not Render Anything </p> */}
                         {downloadedBlogs && typeof downloadedBlogs != "string" && downloadedBlogs.filter((blog: Blog) => {
-                                
-                            
+
+
                             console.log(CategorySelectOutput)
                             console.log(contentType)
                             console.log(DeployedOption)
                             console.log(blog.Category)
-    
+
                             if (CategorySelectOutput != "Select A Category" && CategorySelectOutput != "ALL CATEGORIES") {
                                 if (DeployedOption == "Deployed and Undeployed" || DeployedOption == "Select Your View") {
                                     if (CategorySelectOutput == "Uncategorized") {
-                                        return (blog.Category =="No Category" && blog.ContentType === contentType)
+                                        return (blog.Category == "No Category" && blog.ContentType === contentType)
                                     }
-                                    if (contentType = "all"){
+                                    if (contentType = "all") {
                                         return (blog.Category == CategorySelectOutput)
                                     }
                                     return (blog.Category == CategorySelectOutput && blog.ContentType === contentType)
                                 }
                                 if (DeployedOption == "Deployed Only") {
                                     if (CategorySelectOutput == "Uncategorized") {
-                                        return (blog.Category == "No Category" && blog.ContentType === contentType && blog.Deployed ==true) 
+                                        return (blog.Category == "No Category" && blog.ContentType === contentType && blog.Deployed == true)
                                     }
-                                    if (contentType = "all"){
-                                        return (blog.Category == CategorySelectOutput && blog.Deployed ==true)
+                                    if (contentType = "all") {
+                                        return (blog.Category == CategorySelectOutput && blog.Deployed == true)
                                     }
-                                    return (blog.Category == CategorySelectOutput && blog.ContentType === contentType && blog.Deployed ==true)
+                                    return (blog.Category == CategorySelectOutput && blog.ContentType === contentType && blog.Deployed == true)
                                 }
                                 if (DeployedOption == "Undeployed") {
                                     if (CategorySelectOutput == "Uncategorized") {
                                         return (blog.Category == "No Category" && blog.ContentType === contentType && blog.Deployed == false)
                                     }
-                                    if (contentType = "all"){
-                                        return (blog.Category == CategorySelectOutput && blog.Deployed ==false)
+                                    if (contentType = "all") {
+                                        return (blog.Category == CategorySelectOutput && blog.Deployed == false)
                                     }
                                     return (blog.Category == CategorySelectOutput && blog.ContentType === contentType && blog.Deployed == false)
                                 }
                             }
-                            else { 
+                            else {
                                 console.log(blog.ContentType)
                                 console.log(contentType)
                                 if (DeployedOption == "Deployed and Undeployed" || DeployedOption == "Select Your View") {
@@ -158,9 +158,9 @@ console.log('triggered')
                                         return (blog.Deployed == true)  //edited
                                     }
                                     if (contentType == "uncategorized") {
-                                        return (blog.ContentType === "No Content Type" && blog.Deployed ==true)
+                                        return (blog.ContentType === "No Content Type" && blog.Deployed == true)
                                     }
-                                    return ( blog.ContentType === contentType && blog.Deployed ==true)
+                                    return (blog.ContentType === contentType && blog.Deployed == true)
                                 }
                                 if (DeployedOption == "Undeployed") {
                                     if (contentType == "all") {
@@ -169,30 +169,34 @@ console.log('triggered')
                                     if (contentType == "uncategorized") {
                                         return (blog.ContentType === "No Content Type" && blog.Deployed == false)
                                     }
-                                    return ( blog.ContentType === contentType && blog.Deployed == false)
+                                    return (blog.ContentType === contentType && blog.Deployed == false)
                                 }
                             }
                         }
                         ).map((blog: Blog) => (
-                            <div key={blog.id} className="  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                                <div className="p-5 min-w-[300px] min-h-[50vh] max-w-[50px] max-h-[50px] overflow-y-auto">
-                                    <CloseButton callback={async function () {
-                                        try {
-                                            await deleteMongoDBblog(blog.id);
-                                            serverGetBlogs();
-                                        } catch (error) {
-                                            console.error("Error deleting blog:", error);
-                                            // Optionally handle the error in the UI
-                                        }
-                                    }} >
-                                        <Link href={`/AdminDash/EditBlogByID/${blog.id}`}>
-                                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Title is: {blog.Title}Type is: {blog.BlogType}</h5>
-                                        </Link>
-                                        <div className="mb-3 font-normal text-gray-700 dark:text-gray-400 prose prose-sm">
-                                            <ReactMarkdown>{blog.MarkdownContent}</ReactMarkdown>
-                                        </div>
-                                    </CloseButton >
+                            <div className="flex flex-col gap-2 justify-center items-center ">
+                                <div key={blog.id} className="  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                                    <div className="p-5 min-w-[300px] min-h-[50vh] max-w-[50px] max-h-[50px] overflow-y-auto">
+                                        <CloseButton callback={async function () {
+                                            try {
+                                                await deleteMongoDBblog(blog.id);
+                                                serverGetBlogs();
+                                            } catch (error) {
+                                                console.error("Error deleting blog:", error);
+                                                // Optionally handle the error in the UI
+                                            }
+                                        }} >
+                                            <Link href={`/AdminDash/EditBlogByID/${blog.id}`}>
+                                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Title is: {blog.Title}Type is: {blog.BlogType}</h5>
+                                            </Link>
+                                            <div className="mb-3 font-normal text-gray-700 dark:text-gray-400 prose prose-sm">
+                                                <ReactMarkdown>{blog.MarkdownContent}</ReactMarkdown>
+                                            </div>
+                                        </CloseButton >
+                                    </div>
+
                                 </div>
+                                <a href="#" className="w-full "><button className="btn w-full bg-purple-800 text-white">Converse This Topic With AI - Comming Soon!</button></a>
                             </div>
                         ))}
                     </div>
