d57a2138e52b5d9f16a7508a0ee20eb8fca6d5e3
apophis51 <malcolmxvernon@hotmail.com>
2025-03-13 22:49:38 -0400

    pwa update

---
+++ app/(main site)/(Landing Pages)/Web-Apps/ai-translation/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: "PWA NextJS",
+  description: "It's a simple progressive web application made with NextJS",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "next14", "pwa", "next-pwa"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [
+    {
+      name: "imvinojanv",
+      url: "https://www.linkedin.com/in/imvinojanv/",
+    },
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/icon-128x128.png" },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body >{children}</body>
+    </html>
+  );
+}
